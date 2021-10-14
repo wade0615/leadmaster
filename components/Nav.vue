1c2f6867b92495afb35d1db2e16f8b860a62c582
WadePro <wsw0615@gmail.com>
2021-10-14 17:49:41 +0800

    feat: nav.vue

---
+++ components/Nav.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="nav" class="bg-gray-400">
+    <section
+      class="max-w-screen-xl mx-auto px-6 flex justify-between items-center"
+    >
+      <NuxtLink to="/" class="inline-block">
+        <img src="@/assets/logo.svg" alt="Logo" />
+      </NuxtLink>
+      <div>
+        <v-avatar class="cursor-pointer" @click.stop="drawer = !drawer">
+          <v-icon>mdi-pencil</v-icon>
+        </v-avatar>
+        <div class="member-dropdown inline-block">
+          <a href="#0">
+            <v-avatar>
+              <v-icon dark>
+                mdi-account-circle
+              </v-icon>
+            </v-avatar>
+          </a>
+          <ol class="dropdown-menu">
+            <li class="dropdown-item"><a href="#0">Big Widgets</a></li>
+            <li class="dropdown-item"><a href="#0">Bigger Widgets</a></li>
+            <li class="dropdown-item"><a href="#0">Huge Widgets</a></li>
+          </ol>
+        </div>
+      </div>
+    </section>
+    <section class="p-2">
+      <nav class="mx-auto px-10">
+        <NuxtLink to="/">Home</NuxtLink> |
+        <NuxtLink to="/about">About</NuxtLink> |
+        <NuxtLink to="/login">Login</NuxtLink>
+      </nav>
+    </section>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      temporary
+      right
+    >
+      <div class="px-3 py-2">哈囉是我啦！</div>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/login">Login</NuxtLink>
+        </li>
+      </ul>
+    </v-navigation-drawer>
+    <v-overlay :value="drawer"></v-overlay>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+};
+</script>
+
+<style lang="sass" scope>
+#nav
+  position: sticky
+  top: 0
+  left: 0
+  z-index: 10
+  padding: 1ex 0
+  box-shadow: 0px 0px 16px 2px rgb(79 64 53 / 20%)
+  section:nth-child(1)
+    position: relative
+    &::after
+      content: ""
+      position: absolute
+      top: 100%
+      left: 0
+      width: 100%
+      height: 1px
+      background: #EEE7E1
+    .member-dropdown
+      position: relative
+      &:hover
+        .dropdown-menu
+          display: block
+      a
+        text-decoration: none
+        white-space: nowrap
+        text-overflow: ellipsis
+        color: white
+    .dropdown-menu
+      list-style-type: none
+      margin: 0 auto
+      padding: 0
+      position: absolute
+      width: max-content
+      top: 100%
+      right: 0
+      display: none
+      z-index: 10
+    .dropdown-item
+      padding: 0
+      background: #F67280
+      opacity: 0
+      transform-origin: bottom
+      animation: enter 0.2s ease forwards
+      @for $n from 1 through 3
+        &:nth-child(#{$n})
+          animation-duration: 0.2s + 0.1s * ($n - 1)
+          animation-delay: 0.1s * ($n - 1)
+      &:hover
+        background: #F8B195
+      a
+        display: block
+        padding: 0.75rem 1.25rem
+  section:nth-child(2)
+    overflow-x: scroll
+    nav
+      width: max-content
+      a
+        font-weight: bold
+        color: #2c3e50
+        &.router-link-exact-active
+          color: #fcd846
+  @keyframes enter
+    from
+      opacity: 0
+      transform: scaleY(0.98) translateY(10px)
+    to
+      opacity: 1
+      transform: none
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="london">
+    <Nav v-show="hidden" />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+  watch: {
+    $route() {
+      if (process.client) {
+        this.hideNav();
+      };
+    },
+  },
+  data () {
+    return {
+      hidden: null,
+    }
+  },
+  methods: {
+    isLogin() {
+      return window.location.pathname != '/login';
+    },
+    hideNav() {
+      this.isLogin() ? this.hidden = true : this.hidden = false;
+    },
+  },
+  computed: {
+  },
+  mounted(){
+    this.hideNav();
+  },
+  beforeMount(){},
+  updated(){
+  },
+}
+</script>
+
+<style lang="sass">
+html
+  font-family: 'Noto Serif TC','Source Sans Pro','-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Helvetica Neue','Arial','sans-serif'
+  font-size: 16px
+  // word-spacing: 1px
+  -ms-text-size-adjust: 100%
+  -webkit-text-size-adjust: 100%
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  box-sizing: border-box
+
+*,
+*::before,*::after
+  box-sizing: border-box
+  margin: 0
+</style>
