3ce6f7f864eeb6e9657c700c2c249f7ee83ebbec
WadePro <wsw0615@gmail.com>
2021-10-22 16:20:42 +0800

    feat: ThemeCards.vue

--- components/ThemeCards.vue
+++ components/ThemeCards.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="big-three-photo-frame" class="text-center">
-    <header class="flex flex-col md:flex-row items-center justify-between">
+  <section id="theme-cards" class="text-center">
+    <header class="flex flex-col md:flex-row items-center justify-between mb-8">
       <div class="w-full md:w-auto flex items-top">
         <span class="mr-2 text-xl">/</span>
         <span class="text-2xl">Feature Theme</span>
@@ -10,8 +10,32 @@
         <v-icon>mdi-chevron-right</v-icon>
       </div>
     </header>
-    <main>
-      main
+    <main class="flex flex-col md:flex-row">
+      <div class="mb-10 md:mb-0 md:w-1/3">
+        <img
+          src="https://fakeimg.pl/600x900/?text=Feature Theme"
+          alt="Feature image"
+          class="w-full max-h-80 object-cover"
+        />
+      </div>
+      <div class="flex-1 grid grid-cols-2 md:grid-cols-3 gap-4 md:pl-8">
+        <div v-for="(themePost, index) in themePosts" :key="'themePost' + index"
+          class="theme-posts"
+        >
+          <img
+            :src="themePost"
+            alt="Feature image"
+            class="w-full h-full object-cover"
+          />
+        </div>
+        <div class="theme-posts md:hidden">
+          <img
+            :src="themeLastPost"
+            alt="Feature image"
+            class="w-full h-full object-cover"
+          />
+        </div>
+      </div>
     </main>
   </section>
 </template>
@@ -22,7 +46,14 @@ export default {
   props: [],
   watch: {},
   data() {
-    return {};
+    return {
+      themePosts: [
+        "https://fakeimg.pl/1200x900/?text=themePosts1",
+        "https://fakeimg.pl/1200x900/?text=themePosts2",
+        "https://fakeimg.pl/1200x900/?text=themePosts3",
+      ],
+      themeLastPost: "https://fakeimg.pl/1200x900/?text=themePosts4"
+    };
   },
   computed: {},
   methods: {},
@@ -34,4 +65,12 @@ export default {
 <style lang="sass" scoped>
 #theme-cards
   position: relative
+  .theme-posts
+    position: relative
+    height: 0%
+    padding-top: 100%
+    img
+      position: absolute
+      top: 0
+      left: 0
 </style>
