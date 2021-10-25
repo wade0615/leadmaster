898699ab00205b81c7e8efe1473b14d8c26e230d
WadePro <wsw0615@gmail.com>
2021-10-25 17:15:40 +0800

    feat: slogan.vue

---
+++ components/Slogan.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="slogan relative bg-gray-100 mt-5">
+    <h3 class="text-4xl">{{ title }}</h3>
+    <div class="max-w-screen-xl mx-auto pb-12 px-20">
+      <div class="pt-10 flex flex-col md:flex-row justify-between">
+        <div v-for="(slogan, index) in slogans" :key="'slogan' + index"
+          class="flex-1 p-8"
+        >
+          <v-icon x-large class="mb-5">
+            {{ slogan.icon }}
+          </v-icon>
+          <p class="text-2xl">
+            {{ slogan.text }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "slogan",
+  props: ["title", "slogans"],
+  watch: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  beforeMount() {},
+};
+</script>
+
+<style lang="sass" scoped>
+.slogan
+  position: relative
+  h3
+    position: absolute
+    top: 0
+    left: 50%
+    transform: translate(-50%, -50%)
+</style>
--- pages/Exhibition.vue
+++ pages/Exhibition.vue
@@ -1,12 +1,20 @@
 <template>
   <main id="exhibition" class="text-center">
-    <div class="max-w-screen-xl mx-auto">
-      <Carousel :images="images"
-        class="md:pt-8 md:px-16 mb-16" style="height: 60vh"
-      />
-      <BigThreePhotoFrame class="px-8 md:px-20 pb-5 mb-24" />
-      <ThemeCards class="px-8 md:px-20 mb-5" />
-    </div>
+    <Carousel :images="images"
+      class="max-w-screen-xl mx-auto md:pt-8 md:px-16 mb-16" style="height: 60vh"
+    />
+    <BigThreePhotoFrame class="max-w-screen-xl mx-auto px-8 md:px-20 pb-5 mb-24" />
+    <ThemeCards class="max-w-screen-xl mx-auto px-8 md:px-20 mb-24" />
+    <section class="max-w-screen-xl mx-auto mb-24 text-center">
+      <span class="block mx-auto mb-8 max-w-lg">{{ content }}</span>
+      <v-btn
+        elevation="2"
+        class="px-10 max-w-md"
+      >
+        Click Lorem
+      </v-btn>
+    </section>
+    <Slogan :title="sloganTitle" :slogans="slogans" />
   </main>
 </template>
 
@@ -22,6 +30,22 @@ export default {
         "https://fakeimg.pl/1200x900/?text=David2",
         "https://fakeimg.pl/1200x900/?text=David3",
       ],
+      content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga eum quae iusto reiciendis, nam commodi quisquam eveniet omnis consectetur saepe?",
+      sloganTitle: "Slogan Slogan Slogan",
+      slogans: [
+        {
+          icon: "mdi-wrench",
+          text: "text text text text text text text text text text text text text text text ",
+        },
+        {
+          icon: "mdi-eye",
+          text: "text text text",
+        },
+        {
+          icon: "mdi-lead-pencil",
+          text: "text text text",
+        }
+      ]
     };
   },
   computed: {},
