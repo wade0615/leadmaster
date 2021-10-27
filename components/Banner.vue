4a3e0bae588551afd922c4d576abacfca0e48321
WadePro <wsw0615@gmail.com>
2021-10-27 18:06:21 +0800

    feat: agent banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="banner" class="py-8 text-center">
-    <h2 class="mb-10 text-6xl md:text-8xl">{{ title }}</h2>
+    <h2 class="pb-8 mb-10 text-6xl md:text-8xl">{{ title }}</h2>
     <p class="max-w-screen-md mx-auto">{{ description }}</p>
   </section>
 </template>
