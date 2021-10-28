1582898a77f69c4202dae99f956a4d3af028b7f7
WadePro <wsw0615@gmail.com>
2021-10-28 16:39:13 +0800

    fix: footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,27 @@
 <template>
   <footer id="footer" class="pt-16 pb-8 px-8 bg-gray-400">
     <div class="max-w-screen-xl mx-auto">
-      <section class="mb-3 flex flex-col md:flex-row md:justify-between">
+      <section class="mb-3 flex flex-col md:flex-row md:justify-between md:items-start">
         <img src="@/assets/logo.svg" alt="Logo" class="w-40 mx-auto mb-8 md:m-0" />
-        <div class="flex flex-col md:items-center md:flex-row md:justify-end">
-          <div class="px-10 md:px-4 py-1 md:py-0 text-xl md:text-base">About</div>
-          <div class="px-10 md:px-4 py-1 md:py-0 text-xl md:text-base">Services</div>
-          <div class="px-10 md:px-4 py-1 md:py-0 text-xl md:text-base">Stories</div>
-          <div class="px-10 md:px-4 py-1 md:py-0 text-xl md:text-base">Members</div>
-          <div class="px-10 md:px-4 py-1 md:py-0 text-xl md:text-base">Contact us</div>
+        <div class="">
+          <ul>
+            <li>
+              <v-icon>mdi-facebook</v-icon>
+              <span>Facebook</span>
+            </li>
+            <li>
+              <v-icon>mdi-phone</v-icon>
+              <span>(02)2916-1022</span>
+            </li>
+            <li>
+              <v-icon>mdi-map-marker</v-icon>
+              <span>23141新北市新店區二十張路33巷2弄1號1樓</span>
+            </li>
+          </ul>
         </div>
       </section>
-      <section class="mb-12 px-10 md:px-4 md:text-right">
-        <v-icon large>mdi-facebook</v-icon>
-        <v-icon large>mdi-instagram</v-icon>
-        <v-icon large>mdi-youtube</v-icon>
-      </section>
       <section class="text-center">
-        <span>&copy; 2020-2021, David</span>
+        <span class="text-lg">&copy; 2020-2021, David</span>
       </section>
     </div>
   </footer>
