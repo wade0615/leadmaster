ddaf93427c39e70bf4f4bcdc1fad6cc5577fe5fb
WadePro <wsw0615@gmail.com>
2021-11-02 18:24:45 +0800

    feat: projects data

---
+++ assets/mockdata/projects.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: "Marriott Taipei 台北萬豪酒店",
+    img: "https://media.vogue.com.tw/photos/608a9246385f0fb31ab0b4ca/master/w_1600,h_900,c_limit/14%20Garden%20Kitchen%E5%8D%88%E9%96%93%E9%99%90%E5%AE%9A%E5%8D%8A%E8%87%AA%E5%8A%A9%E9%A4%90_%E5%8F%B0%E5%8C%97%E8%90%AC%E8%B1%AA%E9%85%92%E5%BA%97.jpg",
+    img_alt: "Marriott Taipei 台北萬豪酒店",
+    link: "https://www.taipeimarriott.com.tw/"
+  },
+  {
+    title: "Grand Hyatt Taipei 台北君悅酒店",
+    img: "https://tnimage.s3.hicloud.net.tw/photos/2019/10/02/1570020262-5d949ba6b7f8f.jpg",
+    img_alt: "Grand Hyatt Taipei 台北君悅酒店",
+    link: "https://www.hyatt.com/zh-HK/hotel/taiwan/grand-hyatt-taipei/taigh?src=prop_sm_taigh_grand_facebk_about&fbclid=IwAR2gatFqPOaVxgZeV7lkvfrBLLNs6-2IKeMcX10qjrhZNAae7yDPhWo6h00"
+  },
+  {
+    title: "福容飯店",
+    img: "https://www.fullon-hotels.com.tw/upload/restaurant_list_pic_s/fw_twL_restaurant_20D26_y4meqncxqb.jpg",
+    img_alt: "福容飯店",
+    link: "https://www.fullon-hotels.com.tw/tw/"
+  },
+  {
+    title: "The Sherwood Taipei 台北西華飯店",
+    img: "https://www.sherwood.com.tw/upload/f2e/banner950x1900/tos1.png",
+    img_alt: "The Sherwood Taipei 台北西華飯店",
+    link: "https://www.sherwood.com.tw/zh-tw"
+  },
+  {
+    title: "萬楓酒店",
+    img: "https://cache.marriott.com/marriottassets/marriott/RMQFI/rmqfi-dining-1254-hor-feat.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1180px:*",
+    img_alt: "萬楓酒店",
+    link: "https://www.marriott.com/hotels/travel/rmqfi-fairfield-taichung/"
+  },
+  {
+    title: "國賓飯店",
+    img: "https://www.ambassador-hotels.com/images/files/taipei/dining/a-cut/4V6A1388.jpg",
+    img_alt: "國賓飯店",
+    link: "https://www.ambassador-hotels.com/tc/taipei/"
+  },
+  {
+    title: "W Hotel",
+    img: "https://cache.marriott.com/marriottassets/marriott/TPEWH/tpewh-woobar-6921-hor-feat.jpg?interpolation=progressive-bilinear&downsize=2000px:*",
+    img_alt: "W Hotel",
+    link: "https://www.marriott.com/hotels/travel/tpewh-w-taipei/?scid=5d6c9ba8-fdb3-45cd-90d4-fc3cbfe3681c&y_source=1_Mjc4MjA3My01NTktbG9jYXRpb24ud2Vic2l0ZQ%3D%3D&fbclid=IwAR3TancnMEzau_FIpeG-YuP0j9J3Xf72rQJ95dFY2g7P664CjMzScb0JvvQ"
+  },
+  {
+    title: "大板根森林溫泉酒店",
+    img: "https://www.thegreatroots.com/wp-content/uploads/2016/12/9c6881c2a0f0dbc2550cdf827c6d8bfd.jpg",
+    img_alt: "HOBART",
+    link: "https://www.thegreatroots.com/"
+  },
+]
