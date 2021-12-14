e4f41658ad7c29f9321f044f19de38308b8d206e
Wade <>
2021-12-14 16:58:42 +0800

    feat: coverText animation

--- components/CoverText.vue
+++ components/CoverText.vue
@@ -6,11 +6,13 @@
   <div class="text flex items-center justify-center">
     <article class="p-8 sm:p-20 md:pt-28 max-w-screen-xl mx-auto">
       <p
+        ref="textContent"
         v-html="text"
         class="
           md:w-1/2 text-sm md:text-xl tracking-wider
           p-4 rounded
           bg-gray-50 bg-opacity-80
+          opacity-0
         "
         :class="{'ml-auto': right}"
       ></p>
@@ -28,8 +30,21 @@ export default {
     return {};
   },
   computed: {},
-  methods: {},
-  mounted() {},
+  methods: {
+    showText() {
+      window.addEventListener("scroll", ()=>{
+        const windowScroll = window.innerHeight;
+        const textDOM = this.$refs.textContent;
+        const textOffestTop = this.$refs.textContent.getBoundingClientRect().top;
+        if (windowScroll * 0.8 >= textOffestTop) {
+          textDOM.classList.add("active-animate");
+        };
+      });
+    }
+  },
+  mounted() {
+    this.showText();
+  },
   beforeMount() {},
 };
 </script>
@@ -57,4 +72,15 @@ export default {
     width: 100%
     height: 100%
     z-index: 2
+  .active-animate
+    animation-name: show-text
+    animation-duration: 0.7s
+    animation-fill-mode: forwards
+@keyframes show-text
+  0%
+    transform: translateY(100px)
+    opacity: 0
+  100%
+    transform: translateY(0px)
+    opacity: 1
 </style>
