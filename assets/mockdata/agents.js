9c9c81efb9666f94eada7a3db2f9417f2251c536
Wade <>
2023-04-03 13:41:59 +0800

    update agents GRILLWORK'S photo

--- assets/mockdata/agents.js
+++ assets/mockdata/agents.js
@@ -1,62 +1,62 @@
 export default [
   {
     title: "GRILLWORK'S",
-    img: "https://images.squarespace-cdn.com/content/v1/576ed7249f7456eedf843b98/1484329856462-J5OITD3R46OE6NVN9FR0/BH2_233A-2big.jpg?format=1500w",
+    img: 'https://images.squarespace-cdn.com/content/v1/576ed7249f7456eedf843b98/1576561734625-EODPF46A17JXXADHZ3LP/232Bleecker-4.jpg?format=1500w',
     img_alt: "GRILLWORK'S",
-    link: "https://www.grillworksusa.com/"
+    link: 'https://www.grillworksusa.com/',
   },
   {
-    title: "RATIONAL",
-    img: "https://i.ytimg.com/vi/N5aOlOmhKMk/maxresdefault.jpg",
-    img_alt: "RATIONAL",
-    link: "https://www.rational-online.com/en_sg/home/"
+    title: 'RATIONAL',
+    img: 'https://i.ytimg.com/vi/N5aOlOmhKMk/maxresdefault.jpg',
+    img_alt: 'RATIONAL',
+    link: 'https://www.rational-online.com/en_sg/home/',
   },
   {
-    title: "HOBART",
-    img: "https://cdnimg.webstaurantstore.com/images/guides/945/hobart_trouble_shooting_header.jpg",
-    img_alt: "HOBART",
-    link: "https://www.hobart.ca/"
+    title: 'HOBART',
+    img: 'https://cdnimg.webstaurantstore.com/images/guides/945/hobart_trouble_shooting_header.jpg',
+    img_alt: 'HOBART',
+    link: 'https://www.hobart.ca/',
   },
   {
-    title: "Manitowoc",
-    img: "https://www.manitowocice.com/getattachment/bf4bba52-381e-44c4-a185-b600bd749ac1/image.jpg.aspx?sitename=ManitowocIce&maxsidesize=800&width=800&height=450",
-    img_alt: "Manitowoc",
-    link: "https://www.manitowocice.com/products"
+    title: 'Manitowoc',
+    img: 'https://www.manitowocice.com/getattachment/bf4bba52-381e-44c4-a185-b600bd749ac1/image.jpg.aspx?sitename=ManitowocIce&maxsidesize=800&width=800&height=450',
+    img_alt: 'Manitowoc',
+    link: 'https://www.manitowocice.com/products',
   },
   {
-    title: "Angelo po",
-    img: "https://www.monolithe.it/images/slider_home/img1.jpg",
-    img_alt: "Angelo po",
-    link: "https://www.angelopo.com/en/"
+    title: 'Angelo po',
+    img: 'https://www.monolithe.it/images/slider_home/img1.jpg',
+    img_alt: 'Angelo po',
+    link: 'https://www.angelopo.com/en/',
   },
   {
-    title: "Robot Coupe",
-    img: "https://www.robot-coupe.com/robot-coupe-global/products-family/image-thumb__22137__RBC_cover_540_280/Ambiance%20540x420px-Combine.webp",
-    img_alt: "Robot Coupe",
-    link: "https://www.robot-coupe.com/france/fr"
+    title: 'Robot Coupe',
+    img: 'https://www.robot-coupe.com/robot-coupe-global/products-family/image-thumb__22137__RBC_cover_540_280/Ambiance%20540x420px-Combine.webp',
+    img_alt: 'Robot Coupe',
+    link: 'https://www.robot-coupe.com/france/fr',
   },
   {
-    title: "Cleveland",
-    img: "https://www.welbilt.com/Media/Welbilt-Inc/Our-Brands/Cleveland-2018.jpg",
-    img_alt: "Welbilt Cleveland",
-    link: "https://www.clevelandrange.com/?__hstc=22072164.438bb38c4dd2983cd70767b5736c69da.1635995215710.1635995215710.1635995215710.1&__hssc=22072164.4.1635995215711&__hsfp=3413898664"
+    title: 'Cleveland',
+    img: 'https://www.welbilt.com/Media/Welbilt-Inc/Our-Brands/Cleveland-2018.jpg',
+    img_alt: 'Welbilt Cleveland',
+    link: 'https://www.clevelandrange.com/?__hstc=22072164.438bb38c4dd2983cd70767b5736c69da.1635995215710.1635995215710.1635995215710.1&__hssc=22072164.4.1635995215711&__hsfp=3413898664',
   },
   {
-    title: "SOUTHBEND",
-    img: "https://www.middleby.com/wp-content/uploads/2021/08/Southbend-K-12-Convection-Ovens-Hero-Image.png",
-    img_alt: "SOUTHBEND",
-    link: "https://southbendnc.com/"
+    title: 'SOUTHBEND',
+    img: 'https://www.middleby.com/wp-content/uploads/2021/08/Southbend-K-12-Convection-Ovens-Hero-Image.png',
+    img_alt: 'SOUTHBEND',
+    link: 'https://southbendnc.com/',
   },
   {
-    title: "Blodgett",
-    img: "http://blog.etundra.com/wp-content/Media/2014/11/blodgett-hydrovection-oven.jpg",
-    img_alt: "Blodgett",
-    link: "https://www.blodgett.com/"
+    title: 'Blodgett',
+    img: 'http://blog.etundra.com/wp-content/Media/2014/11/blodgett-hydrovection-oven.jpg',
+    img_alt: 'Blodgett',
+    link: 'https://www.blodgett.com/',
   },
   {
-    title: "Pitco",
-    img: "https://www.middleby.com/wp-content/uploads/2021/08/Middleby-Pitco-CU-Hero-Image.png",
-    img_alt: "Pitco",
-    link: "https://www.pitco.com/"
+    title: 'Pitco',
+    img: 'https://www.middleby.com/wp-content/uploads/2021/08/Middleby-Pitco-CU-Hero-Image.png',
+    img_alt: 'Pitco',
+    link: 'https://www.pitco.com/',
   },
 ]
