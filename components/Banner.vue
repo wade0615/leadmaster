b716d89133769c71ecbba7ae0377de36cccaa367
Wade <>
2023-04-05 17:06:11 +0800

    feat: update intro text

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,23 +1,23 @@
 <template>
   <section id="banner" class="py-8 text-center">
     <h2 class="pb-8 mb-10 text-6xl md:text-8xl">{{ title }}</h2>
-    <p class="max-w-screen-md mx-auto text-lg">{{ description }}</p>
+    <p class="max-w-screen-md mx-auto text-lg" v-html="description"></p>
   </section>
 </template>
 
 <script>
 export default {
-  name: "Banner",
-  props: ["title", "description"],
+  name: 'Banner',
+  props: ['title', 'description'],
   watch: {},
   data() {
-    return {};
+    return {}
   },
   computed: {},
   methods: {},
   mounted() {},
   beforeMount() {},
-};
+}
 </script>
 
 <style lang="sass" scoped>
